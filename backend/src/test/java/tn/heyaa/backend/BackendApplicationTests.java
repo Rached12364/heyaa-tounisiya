5f9490371decb983d7b2093302027fcf13c6fd72
RachedILAHI <rached.ilahi@enicar.ucar.tn>
2026-07-04 09:49:27 +0100

    🚀 init: Angular frontend + Spring Boot backend - Heyaa Tounisiya

---
+++ backend/src/test/java/tn/heyaa/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.heyaa.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
